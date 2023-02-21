b783f17019cf3a3a312c8a518e025144557a6051
mdjahidhasan009 <jahid60012@gmail.com>
2023-02-22 01:59:23 +0600

    add message input field functionality & call api to create message

--- src/components/messages/MessageInputField.tsx
+++ src/components/messages/MessageInputField.tsx
@@ -1,9 +1,25 @@
-import {MessageInput, MessageInputContainer} from "../../utils/styles";
+import { MessageInput, MessageInputContainer } from "../../utils/styles";
+import React, {Dispatch, FC, SetStateAction} from "react";
 
-export const MessageInputField = () => {
+type Props = {
+  content: string;
+  setContent: Dispatch<SetStateAction<string>>;
+  sendMessage: (e: React.FormEvent<HTMLFormElement>) => void;
+}
+
+export const MessageInputField: FC<Props> = ({
+  content,
+  setContent,
+  sendMessage
+}) => {
   return (
     <MessageInputContainer>
-      <MessageInput />
+      <form onSubmit={sendMessage}>
+        <MessageInput
+          value={content}
+          onChange={(e)=> setContent(e.target.value)}
+        />
+      </form>
 
     </MessageInputContainer>
   )
--- src/components/messages/MessagePanel.tsx
+++ src/components/messages/MessagePanel.tsx
@@ -1,22 +1,46 @@
 import {MessageInput, MessagePanelBody, MessagePanelStyle} from "../../utils/styles";
 import { MessageContainer } from "./MessageContainer";
 import { MessageType } from "../../utils/types";
-import {FC} from "react";
+import React, {FC, useState} from "react";
 import MessagePanelHeader from "./MessagePanelHeader";
 import {MessageInputField} from "./MessageInputField";
+import {useParams} from "react-router-dom";
+import {postNewMessage} from "../../utils/api";
 
 type Props = {
   messages: MessageType[];
 }
 
 export const MessagePanel: FC<Props> = ({ messages }) => {
+
+  const [ content, setContent ] = useState('');
+  const { id } = useParams();
+  const sendMessage = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(id);
+    console.log('Sending Message', content);
+    if(!id || !content) return;
+    const conversationId = parseInt(id);
+
+    try {
+      await postNewMessage({ conversationId, content });
+      setContent('');
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
   return (
     <>
       <MessagePanelHeader />
       <MessagePanelStyle>
       <MessagePanelBody>
         <MessageContainer messages={messages} />
-        <MessageInputField />
+        <MessageInputField
+          content={content}
+          setContent={setContent}
+          sendMessage={sendMessage}
+        />
       </MessagePanelBody>
     </MessagePanelStyle>
     </>
