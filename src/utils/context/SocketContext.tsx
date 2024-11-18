64fa8b9aa7a94750ed2aa14c686ee9cd45f98c24
MD Jahid Hasan <63992657+mdjahidhasan009@users.noreply.github.com>
2024-11-19 02:04:00 +0600

    feat(socket): enable WebSocket and polling transport support for Socket.IO client

--- src/utils/context/SocketContext.tsx
+++ src/utils/context/SocketContext.tsx
@@ -2,6 +2,7 @@ import { io } from 'socket.io-client';
 import { createContext } from "react";
 
 export const socket = io(process.env.REACT_APP_WEBSOCKET_URL!, {
+  transports: ["websocket", "polling"],
   withCredentials: true,
 });
 export const SocketContext = createContext(socket);
